99d290a725ca1ea9906ba64eaae5d84fc450c27c
PendalF <pendalf@mail.ru>
2019-01-30 22:15:13 +0300

    Change https to http for xmlns

--- src/Sitemap.php
+++ src/Sitemap.php
@@ -86,7 +86,7 @@ class Sitemap extends BaseObject
 		});
 		$priorityStep = 1 / count($this->indexSitemap);
 		$priority     = 1;
-		$str          = '<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">';
+		$str          = '<?xml version="1.0" encoding="UTF-8"?><sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 		foreach ($this->indexSitemap as $item) {
 			$str .= '<sitemap><loc>' . $this->domain . '/' . basename($item['file']) . ($this->compressWithGzip ? '.gz' : '') . '</loc>';
 			if ($item['date']) {
@@ -156,7 +156,7 @@ class Sitemap extends BaseObject
 	{
 		$priorityStep = 1 / count($items);
 		$priority     = 1;
-		$str          = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">';
+		$str          = '<?xml version="1.0" encoding="UTF-8"?><urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
 		foreach ($items as $item) {
 			$str .= '<url><loc>' . $this->domain . $sitemap->getUrl($item) . '</loc>';
 			if ($lastmod = $sitemap->getLastmod($item)) {
